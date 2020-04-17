3e9ae35dacfecf02b9d9afefc1358216d7e3b479
Hugo <ophugo@hotmail.com>
2020-04-16 19:15:17 -0600

    Initial Commit

---
+++ ReviewCodeActivity6/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ReviewCodeActivity6
+//
+//  Created by Hugo A Valencia on 4/16/20.
+//  Copyright © 2020 Hugo A Valencia. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
